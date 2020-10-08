a27f8c73572c32e22ef9a9264fd23928cdd64180
sophie-apples <sophieannemurray@gmail.com>
2020-10-08 13:36:33 +0000

    food changes position randomly

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 //Game Loop
-import { update as updateSnake, draw as drawSnake, snake_speed } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
 
 import { update as updateFood, draw as drawFood } from './food.js'
 
@@ -10,7 +10,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / snake_speed) return
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
     console.log('Render')
     lastRenderTime = currentTime
